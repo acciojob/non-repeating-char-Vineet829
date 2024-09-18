4700d56b8a0874dd57df8e3b4c99e8f7e7eb2b31
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-19 03:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  for(let i of str){
+	  if(str.indexOf(i) === str.lastIndexOf(i)){
+		  return i
+	  }
+  }
+return null  
 }
+	
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
